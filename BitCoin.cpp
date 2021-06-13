50bcedc85c2896c2ee297fcf7efe48fc3f956f12
Chetas Sharma <sharmachetas.111@gmail.com>
2021-06-13 21:58:10 +0530

    included both sorting of fee and weigth in single function

--- BitCoin.cpp
+++ BitCoin.cpp
@@ -10,8 +10,8 @@ using namespace std;
 const int maxWeight = 4000000;
 const int entries = 5214;
 
-// comparator function to comare transactions for sorting
-bool compareTransaction(Transaction t1, Transaction t2){
+// descending order of fee
+bool compareTransactionMaxFee(Transaction t1, Transaction t2){
     if(t1.getFee() < t2.getFee()){
         return false;
     }
@@ -24,6 +24,24 @@ bool compareTransaction(Transaction t1, Transaction t2){
         }
         else{
             return false;
+        }
+    }
+}
+
+// ascending order of weight
+bool compareTransactionMinWeight(Transaction t1, Transaction t2){
+    if(t1.getWeight() < t2.getWeight()){
+        return true;
+    }
+    else if(t1.getWeight() > t2.getWeight()){
+        return false;
+    }
+    else{
+        if(t1.getFee() < t2.getFee()){
+            return false;
+        }
+        else{
+            return true;
         }
     }
 }
@@ -60,11 +78,39 @@ vector<Transaction> transactionFileParser(string fileName, int numberOfEntries){
     return data;
 }
 
+// for sorting the transactions 
+void sortTransactions(vector<Transaction> &dataFee, vector<Transaction> &dataWeight){
+    int n = dataFee.size();
+    for(int i=0;i<n;i++){
+        Transaction current_max_fee = dataFee[i], current_min_weight = dataWeight[i];
+        int swapPosition_fee = i, swapPosition_weight = i;
+        for(int j=i+1;j<n;j++){
+            if(!compareTransactionMaxFee(current_max_fee,dataFee[j])){
+                current_max_fee = dataFee[j];
+                swapPosition_fee = j;
+            }
+            if(!compareTransactionMinWeight(current_min_weight,dataWeight[j])){
+                current_min_weight = dataWeight[j];
+                swapPosition_weight = j;
+            }
+        }
+        dataFee[swapPosition_fee] = dataFee[i];
+        dataFee[i] = current_max_fee;
+        dataWeight[swapPosition_weight] = dataWeight[i];
+        dataWeight[i] = current_min_weight;
+    }
+}
+
 int main(){
-    vector<Transaction> data = transactionFileParser("mempool.csv", entries);
-    // for(int i=0;i<data.size();i++){
-    //     cout<<i+1<<" : ";
-    //     data[i].printDetails();
-    // }
+    vector<Transaction> dataSortedForFee = transactionFileParser("mempool.csv", entries);
+    vector<Transaction> dataSortedForWeight = dataSortedForFee;
+    sortTransactions(dataSortedForFee,dataSortedForWeight);
+    for(int i=0;i<dataSortedForFee.size();i++){
+        cout<<i+1<<"-> ";
+        cout<<"Sorted Fee :"<<dataSortedForFee[i].getFee()<<" "<<dataSortedForFee[i].getWeight()<<"  ";
+        cout<<"Sorted Weight :"<<dataSortedForWeight[i].getFee()<<" "<<dataSortedForWeight[i].getWeight()<<"  ";
+        cout<<endl;
+
+    }
     // cout<<data.size()<<endl;
 }
